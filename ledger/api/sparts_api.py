2b8d2f2644361171c9ea3cf213d9b10a7ef211c3
SamAhm <sameer.farooq@gmail.com>
2017-09-14 19:56:35 -0700

    Update sparts_api.py

--- ledger/api/sparts_api.py
+++ ledger/api/sparts_api.py
@@ -1,4 +1,5 @@
-#copyright 2017 Wind River Systems
+# ------------------------------------------------------------------------------
+# copyright 2017 Wind River Systems
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
